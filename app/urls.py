83fce60d02eecd3f5f578828b4b287a595a3dc0f
GGS_1SLOM <i46590587@gmail.com>
2026-07-02 12:48:00 +0500

    index va main tugadi

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
-
-from django import views
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path("", views.index, name="index"),
+    path("update/<int:id>/", views.update, name="update"),
+    path("delete/<int:id>/", views.delete, name="delete"),  # delete view yozishingiz kerak
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from app.models import MAC
 
 # Create your views here.
+
+
+from django.shortcuts import render, redirect
+from .models import MAC
+
+def home(request):
+    if request.method == "POST":
+        MAC.objects.create(
+            nimi=request.POST.get("nimi"),
+            narxi=request.POST.get("narxi"),
+            image_url=request.POST.get("image_url"),
+        )
+        return redirect("/")
+
+    macs = MAC.objects.all()
+    return render(request, "index.html", {"macs": macs})
