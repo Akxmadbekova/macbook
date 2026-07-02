5924e5cac420dcff2fa8ebc62c5ca72188c1b071
GGS_1SLOM <i46590587@gmail.com>
2026-07-02 12:36:58 +0500

    index va main tugadi

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class MAC(models.Model):
+    nimi = models.CharField(max_length=100)
+    narxi = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(max_length=200)
+
+
+    def __str__(self):
+        return self.title
